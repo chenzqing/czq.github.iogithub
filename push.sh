26d7669463279645e0e2fc4a9aec25c702b16bab
chenzhiqing <2531074282@qq.com>
2021-02-13 21:43:03 +0800

    33

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git rm ./cards/*
 git add .
 mkdir cards
 git add cards
-git commit -m "11 "
+git commit -m "33 "
 git push
 
 python game.py
@@ -13,5 +13,5 @@ git add cards/2.jpg
 git add cards/3.jpg
 git add cards/4.jpg
 git add cards/5.jpg
-git commit -m "22"
+git commit -m "44"
 git push
